5b4fec140d3446277c49a0039065f2e5c6fbbd73
chmk21039 <114901367+chmk21039@users.noreply.github.com>
2022-11-07 16:01:15 +0900

    Update main07.js

--- webp-excercise1/scripts/main07.js
+++ webp-excercise1/scripts/main07.js
@@ -53,6 +53,21 @@ Ball.prototype.update = function() {
   this.x += this.velX;
   this.y += this.velY;
 }
+
+Ball.prototype.collisionDetect = function() {
+  for (let j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+      }
+    }
+  }
+}
+
 let balls = [];
 
 while (balls.length < 25) {
@@ -77,6 +92,7 @@ function loop() {
   for (let i = 0; i < balls.length; i++) {
     balls[i].draw();
     balls[i].update();
+    balls[i].collisionDetect();
   }
 
   requestAnimationFrame(loop);
